e960ded1c4b3bcdf5842745c317eb9dfbf77b3f8
saman <saman.sahebi@gmail.com>
2024-12-03 03:28:51 +0330

    init

--- serializers.py
+++ serializers.py
@@ -4,7 +4,3 @@ from pydantic import BaseModel
 class AuthenticateByUserPass(BaseModel):
     username: str
     password: str
-
-
-class GetAllRepositories(BaseModel):
-    git_token: str
--- views.py
+++ views.py
@@ -1,6 +1,5 @@
-from fastapi import APIRouter, HTTPException, Header
 import aiohttp
-from serializers import GetAllRepositories
+from fastapi import APIRouter, Request, HTTPException
 from github import Github, Auth
 from serializers import AuthenticateByUserPass
 
@@ -11,12 +10,38 @@ router = APIRouter()
 @router.post("/authenticate")
 async def authenticate_user(authenticate: AuthenticateByUserPass):
     auth = Auth.Login(authenticate.username, authenticate.password)
+    g = Github(auth=auth)
+    g.get_user()
     return {"token": f"{auth.token}"}
 
 
-@router.post("/get-all-repositories")
-async def get_all_repositories(repositories: GetAllRepositories):
-    auth = Auth.Token(token=repositories.git_token)
-    g = Github(auth=auth)
+@router.get("/orgs/{org}/repos")
+async def get_all_repositories(request: Request, org: str):
+    async with aiohttp.ClientSession() as session:
+        url = f"https://api.github.com/orgs/{org}/repos"
+        token = request.headers.get("github-token", None)
+        session.headers.update({"Accept": "application/vnd.github+json"})
+        session.headers.update({"Authorization": f"Bearer {token}"})
+        session.headers.update({"X-GitHub-Api-Version": "2022-11-28"})
+        async with session.get(url) as resp:
+            if resp.status == 200:
+                data = await resp.json()
+                return data
+            else:
+                raise HTTPException(status_code=resp.status, detail=resp.reason)
 
-    return {"repositories": g.get_repos()}
+
+@router.get("/repos/{owner}/{repo}")
+async def get_all_repositories(request: Request, owner: str, repo: str):
+    async with aiohttp.ClientSession() as session:
+        url = f"https://api.github.com/repos/{owner}/{repo}"
+        token = request.headers.get("github-token", None)
+        session.headers.update({"Accept": "application/vnd.github+json"})
+        session.headers.update({"Authorization": f"Bearer {token}"})
+        session.headers.update({"X-GitHub-Api-Version": "2022-11-28"})
+        async with session.get(url) as resp:
+            if resp.status == 200:
+                data = await resp.json()
+                return data
+            else:
+                raise HTTPException(status_code=resp.status, detail=resp.reason)
