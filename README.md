2d46d5ca68acbbdca21ded6e1bb0e9639eb9e668
saman <saman.sahebi@gmail.com>
2024-12-06 13:59:51 +0330

    get file content and get commit implemented

---
+++ README.md
@@ -0,0 +1,16 @@
+# Github Integration
+
+
+## Get Started:
+
+```
+python -m venv venv
+pip install -r requirements.txt
+python main.py
+```
+
+## Run With Docker:
+
+```
+sudo docker compose -f docker-compose.yml build --up
+```
--- views.py
+++ views.py
@@ -45,3 +45,51 @@ async def get_all_repositories(request: Request, owner: str, repo: str):
                 return data
             else:
                 raise HTTPException(status_code=resp.status, detail=resp.reason)
+
+
+@router.get("/{user}/repos")
+async def get_all_repositories(request: Request, user: str):
+    async with aiohttp.ClientSession() as session:
+        url = f"https://api.github.com/users/{user}/repos"
+        token = request.headers.get("github-token", None)
+        session.headers.update({"Accept": "application/vnd.github+json"})
+        session.headers.update({"Authorization": f"Bearer {token}"})
+        session.headers.update({"X-GitHub-Api-Version": "2022-11-28"})
+        async with session.get(url) as resp:
+            if resp.status == 200:
+                data = await resp.json()
+                return data
+            else:
+                raise HTTPException(status_code=resp.status, detail=resp.reason)
+
+
+@router.get("/repos/{owner}/{repo}/contents/{path}")
+async def get_all_repositories(request: Request, owner: str, repo: str, path: str):
+    async with aiohttp.ClientSession() as session:
+        url = f"https://api.github.com/repos/{owner}/{repo}/contents/{path}"
+        token = request.headers.get("github-token", None)
+        session.headers.update({"Accept": "application/vnd.github.object+json"})
+        session.headers.update({"Authorization": f"Bearer {token}"})
+        session.headers.update({"X-GitHub-Api-Version": "2022-11-28"})
+        async with session.get(url) as resp:
+            if resp.status == 200:
+                data = await resp.json()
+                return data
+            else:
+                raise HTTPException(status_code=resp.status, detail=resp.reason)
+
+
+@router.get("/repos/{owner}/{repo}/commits/{ref}")
+async def get_all_repositories(request: Request, owner: str, repo: str, ref: str):
+    async with aiohttp.ClientSession() as session:
+        url = f"https://api.github.com/repos/{owner}/{repo}/commits/{ref}"
+        token = request.headers.get("github-token", None)
+        session.headers.update({"Accept": "application/vnd.github+json"})
+        session.headers.update({"Authorization": f"Bearer {token}"})
+        session.headers.update({"X-GitHub-Api-Version": "2022-11-28"})
+        async with session.get(url) as resp:
+            if resp.status == 200:
+                data = await resp.json()
+                return data
+            else:
+                raise HTTPException(status_code=resp.status, detail=resp.reason)
